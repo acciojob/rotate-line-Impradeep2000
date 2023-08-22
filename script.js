9814279ed22ae3173a5a4754671ee4e0dc89e4cf
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-08-22 19:40:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const div = document.createElement("div")
+div.style.position = "absolute"
+div.id = "line"
+
+let rotation = 0;
+setInterval(()=>{
+	rotation +=5;
+	div.style.transform = `translate(-50%,-50%) rotate(${rotation}deg)`
+},50)
+document.body.appendChild(div)
